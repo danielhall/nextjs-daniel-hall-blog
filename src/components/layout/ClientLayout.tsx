6c8d43fd6193f1f56532f5f3c46b507c54d63f4f
Daniel Hall <daniel.jh310@gmail.com>
2025-05-20 23:56:28 +0100

    Add new icons and layout components for improved UI
    - Added apple-touch-icon.png, favicon-16x16.png, favicon-32x32.png, and favicon.svg for enhanced branding and compatibility across devices.
    - Created a site.webmanifest for PWA support with specified icons and colors.
    - Introduced a new icon.svg in the app directory for consistent branding.
    - Implemented ClientLayout component to manage sidebar and header layout, enhancing the overall user experience.

---
+++ src/components/layout/ClientLayout.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useState } from "react";
+import { Outfit } from "next/font/google";
+import Sidebar from "@/components/sidebar";
+import Header from "@/components/header/header";
+
+const geistSans = Outfit({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export default function ClientLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className={`${geistSans.variable} flex h-screen font-geistSans`}>
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
+      {/* Main Content */}
+      <main className="flex-1 bg-background overflow-y-auto px-6 pt-6 sm:pt-10 lg:pt-3 pb-12">
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
+        <div className="container pt-2 px-4">
+          {children}
+        </div>
+      </main>
+    </div>
+  );
+}
