25c53206cc936d155f525dcb1e92bd3c7b13ea2d
Daniel Hall <daniel.jh310@gmail.com>
2025-05-26 20:35:08 +0100

    fix: update layout and styling of Header component for consistency

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -10,16 +10,23 @@ import { TbLayoutSidebarLeftExpand } from "react-icons/tb";
 const Header = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => { 
     return (
         <>
-            <div className={`flex items-center px-4 md:px-8 py-4 md:hidden transition-opacity  
-                    ${sidebarOpen ? "opacity-10 duration-100" : "opacity-100 duration-700"}`}>
-                <a onClick={() => setSidebarOpen(true)} className="inline-flex items-center">
-                    <TbLayoutSidebarLeftExpand className="w-8 h-8 bg-navigation p-2 rounded" />
-                </a>
-                <a onClick={() => setSidebarOpen(true)} className="px-3 rounded inline-block">
-                    <span className="text-2xl font-bold block">Daniel Hall</span>
-                    <span className="text-sm font-italic block">Software Solutions Architect</span>
-                </a>
-            </div>
+        <div className={`flex items-top ml-2 transition-opacity md:hidden ${sidebarOpen ? "opacity-10 duration-100" : "opacity-100 duration-700"}`}>
+          <a
+            onClick={() => setSidebarOpen(!sidebarOpen)}
+            className="inline-block flex items-top mt-4 md:hidden"
+          >
+            <TbLayoutSidebarLeftExpand className="w-8 h-8 bg-navigation p-2 rounded" />
+          </a>
+          <a
+            onClick={() => setSidebarOpen(!sidebarOpen)}
+            className="p-2 m-2 md:ml-0 md:pl-0 rounded inline-block"
+          >
+            <span className="text-2xl font-bold block">Daniel Hall</span>
+            <span className="text-sm font-italic block">
+              Software Solutions Architect
+            </span>
+          </a>
+        </div>
             
         </>
     )
